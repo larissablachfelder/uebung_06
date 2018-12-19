539f4d834dc2815a9534ee1416e200051620b378
Larissa Blachfelder <1810653996@stud.fh-kufstein.ac.at>
2018-12-19 10:29:31 +0100

    aufgabe 3

--- src/Aufgabe_3/Aufruf3.java
+++ src/Aufgabe_3/Aufruf3.java
@@ -1,4 +1,14 @@
 package Aufgabe_3;
 
-public class Aufruf3 {
+public class Aufruf3
+{
+    public static void main(String[] args)
+    {
+        int[] array = {1,2,3,4,5,6,7,8};
+
+        /*for(Integer a : array)
+        {
+            System.out.println(a);
+        }*/
+    }
 }
---
+++ src/Aufgabe_4/Aufruf4.java
@@ -0,0 +1,4 @@
+package Aufgabe_4;
+
+public class Aufruf4 {
+}
