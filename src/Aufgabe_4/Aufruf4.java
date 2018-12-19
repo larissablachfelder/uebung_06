4203aec74ade1b10341a5a2798f53246bd98554d
Larissa Blachfelder <1810653996@stud.fh-kufstein.ac.at>
2018-12-19 11:18:39 +0100

    aufgabe 4

--- src/Aufgabe_4/Aufruf4.java
+++ src/Aufgabe_4/Aufruf4.java
@@ -1,4 +1,32 @@
 package Aufgabe_4;
 
-public class Aufruf4 {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static java.lang.Math.random;
+
+public class Aufruf4
+{
+    public static void main(String[] args) {
+
+        List<Integer> liste = new ArrayList<>();
+        Random zufallszahl = new Random();
+        int zz = zufallszahl.nextInt(100)+1;
+
+
+        for (int zahl = 0; zahl < zz; zahl++)
+        {
+            liste.add(zahl+zz);
+
+        }
+
+        System.out.println(liste.size());
+
+        for (Integer i : liste)
+        {
+            System.out.println(i);
+        }
+
+    }
 }
